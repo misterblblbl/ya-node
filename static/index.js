cbc5412189190ff32f29aee231d86a4ad5662e31
Alexandra Godun <mister.blblbl@gmail.com>
2017-08-27 19:48:00 +0300

    Rewrite form object in functional style

--- static/index.js
+++ static/index.js
@@ -1,26 +1,24 @@
 'use strict';
 
 const MyForm = (() => {
-  const sumNumbers = str => {
-    const numbers = str.split('').filter(item => {
-      const parsedNumber = Number.parseInt(item);
-        return !Number.isNaN(parsedNumber); 
-      });
-  
-      return numbers.reduce((acc, x) => {
-        return acc + Number.parseInt(x);
-      }, 0);
-    }
-  const validateByPattern = pattern => value => pattern.test(value);
-  const checkTotalNumbers = value => sumNumbers(value) < 30;
-
   const MyForm = function(form) {
-    this.form = form;
-    this.action = form.action;
-    this.formResult = form.querySelector('#resultContainer');
-    this.submitBtn = form.querySelector('#submitButton');
-  
-    this.validationRules = [
+    const action = form.action;
+    const formResult = form.querySelector('#resultContainer');
+    const submitBtn = form.querySelector('#submitButton');
+    const sumNumbers = str => {
+      const numbers = str.split('').filter(item => {
+        const parsedNumber = Number.parseInt(item);
+          return !Number.isNaN(parsedNumber); 
+        });
+    
+        return numbers.reduce((acc, x) => {
+          return acc + Number.parseInt(x);
+        }, 0);
+      }
+    const validateByPattern = pattern => value => pattern.test(value);
+    const checkTotalNumbers = value => sumNumbers(value) < 30;
+
+    const validationRules = [
       {
         id: 'inputFio',
         predicates: [
@@ -44,116 +42,108 @@ const MyForm = (() => {
         error: 'Invalid phone'
       },
     ];
-  
-    this.validate = this.validate.bind(this);
-    this.submit = this.submit.bind(this);
-    this.getData = this.getData.bind(this);
-    this.setData = this.setData.bind(this);
-    this.sendRequest = this.sendRequest.bind(this);
-    this.modifyFormState = this.modifyFormState.bind(this);
-  
-    form.addEventListener('submit', this.submit);
-  }
-  
-  MyForm.prototype.setData = function(data) {
-    const inputs = this.form.querySelectorAll('input');
-    Array.prototype.forEach.call(inputs, x => {
-      x.value = data[x.id] || '';
-    })
-  }
-  
-  MyForm.prototype.getData = function() {
-    const inputs = this.form.querySelectorAll('input');
-  
-    return Array.prototype.reduce.call(inputs, (acc, x) => {
-      acc[x.id] = x.value;
-      return acc;
-    }, {});
-  }
-  
-  MyForm.prototype.validate = function() {
-    const data = this.getData();
-  
-    const errors = this.validationRules.reduce((acc, x) => {
-      const value = data[x.id];
-      const isFieldValid = x.predicates.every(predicate => predicate(value));
 
-      if (!isFieldValid) {
-        this.form[x.id].classList.add('form__input--error');
-        return acc.concat([x.error]);
-      }
-  
-      return acc;
-    }, []);
-  
-    return {
-      isValid: errors.length === 0, 
-      errorFields: errors
-    };
-  }
-
-  MyForm.prototype.modifyFormState = function(status, message) {
-    const resultClass = {
-      success: 'form__result--success',
-      error: 'form__result--error',
-      progress: 'form__result--progress'
-    };
-
-    // it's okay, classList.remove doesn't throw exception if class doesn't exist
-    this.formResult.classList.remove(resultClass.progress);
-    this.formResult.classList.add(resultClass[status]);
-
-    if (status === 'error') {
-      const errorMessage = document.createElement('div');
-      errorMessage.classList.add('form__error-message');
-      errorMessage.textContent = message;
-      this.formResult.appendChild(errorMessage);
+    this.setData = data => {
+      const inputs = form.querySelectorAll('input');
+      Array.prototype.forEach.call(inputs, x => {
+        x.value = data[x.id] || '';
+      })
     }
-  }
+    
+    this.getData = () => {
+      const inputs = form.querySelectorAll('input');
+    
+      return Array.prototype.reduce.call(inputs, (acc, x) => {
+        acc[x.id] = x.value;
+        return acc;
+      }, {});
+    }
+    
+    this.validate = () => {
+      const data = this.getData();
+    
+      const errors = validationRules.reduce((acc, x) => {
+        const value = data[x.id];
+        const isFieldValid = x.predicates.every(predicate => predicate(value));
   
-  MyForm.prototype.sendRequest = function(body) {
-    const self = this;
-    const xhr = new XMLHttpRequest();
-    console.log('Request to ' + this.action);
+        if (!isFieldValid) {
+          form[x.id].classList.add('form__input--error');
+          return acc.concat([x.error]);
+        }
+    
+        return acc;
+      }, []);
+    
+      return {
+        isValid: errors.length === 0, 
+        errorFields: errors
+      };
+    }
   
-    xhr.open("POST", self.action, true);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    const modifyFormState = (status, message) => {
+      const resultClass = {
+        success: 'form__result--success',
+        error: 'form__result--error',
+        progress: 'form__result--progress'
+      };
   
-    xhr.onreadystatechange = function() {
-      if (this.readyState === 4 && this.response) {
-        const parsedResponse = JSON.parse(this.response);
-        const { status, timeout, reason } = parsedResponse;
-        self.modifyFormState(status, reason);
-
-        if (status === 'progress') {
-          setTimeout(self.sendRequest, timeout, body);
+      // it's okay, classList.remove doesn't throw exception if class doesn't exist
+      formResult.classList.remove(resultClass.progress);
+      formResult.classList.add(resultClass[status]);
+  
+      if (status === 'error') {
+        const errorMessage = document.createElement('div');
+        errorMessage.classList.add('form__error-message');
+        errorMessage.textContent = message;
+        formResult.appendChild(errorMessage);
+      }
+    };
+    
+    const sendRequest = body => {
+      const xhr = new XMLHttpRequest();
+      console.log('Sending request to ' + action);
+      
+      xhr.open("POST", action, true);
+      xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+      
+      xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.response) {
+          const parsedResponse = JSON.parse(this.response);
+          const { status, timeout, reason } = parsedResponse;
+          modifyFormState(status, reason);
+  
+          if (status === 'progress') {
+            setTimeout(sendRequest, timeout, body);
+          }
         }
       }
+    
+      xhr.send(body);
     }
+    
+    this.submit = event => {
+      event && event.preventDefault();
+    
+      const { isValid } = this.validate();
+      const data = this.getData();
+      const queryParams = Object.keys(data).reduce((acc, x) => {
+        if (data[x]) {
+          return acc.concat(form[x].name + '=' + encodeURIComponent(data[x]));
+        }
+        return acc;
+      }, []);
+      const body = queryParams.join('&');
   
-    xhr.send(body);
-  }
-  
-  MyForm.prototype.submit = function(event) {
-    event && event.preventDefault();
-  
-    const { isValid } = this.validate();
-    const data = this.getData();
-    const queryParams = Object.keys(data).reduce((acc, x) => {
-      if (data[x]) {
-        return acc.concat(this.form[x].name + '=' + encodeURIComponent(data[x]));
+      if (isValid) {
+        submitBtn.classList.add('form__btn--disabled');
+        submitBtn.disabled = true;
+        
+        sendRequest(body);
       }
-      return acc;
-    }, []);
-    const body = queryParams.join('&');
+    };
 
-    if (isValid) {
-      this.submitBtn.classList.add('form__btn--disabled');
-      this.submitBtn.disabled = true;
-      
-      this.sendRequest(body);
-    }
-  }
+    form.addEventListener('submit', this.submit);
+  };
 
   return MyForm;
 })();
